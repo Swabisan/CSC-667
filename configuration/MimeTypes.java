9d14a0ee3e50418c84f7c8eae69b3e9dac87ab00
James Andrews <bdaddyja@gmail.com>
2018-09-08 17:38:58 -0700

    Loads mimeTypes to hashmap and retrieves via lookup method.

---
+++ configuration/MimeTypes.java
@@ -0,0 +1,51 @@
+
+package configuration;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class MimeTypes implements Config {
+
+  private String config;
+  private HashMap<String, String> mimeTypeMap = new HashMap<String, String>();
+  private BufferedReader bufferReader;
+  private FileReader fileReader;
+  private StringTokenizer token;
+  private String sCurrentLine;
+  private String key;
+  private String value;
+
+  public MimeTypes(String filePath) {
+    this.config = filePath;
+    this.load();
+  }
+
+  public void load() {
+    try {
+      this.fileReader   = new FileReader(config);
+      this.bufferReader = new BufferedReader(fileReader);
+
+      while((sCurrentLine = this.bufferReader.readLine()) != null) {
+        this.token = new StringTokenizer(sCurrentLine);
+
+        if(this.token.countTokens() > 1) {
+          this.value = this.token.nextToken();
+
+          while(this.token.hasMoreTokens()) {
+            this.key = this.token.nextToken();
+            mimeTypeMap.put(this.key,this.value);
+          }
+        }
+      }
+    } catch(IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public String lookUp(String mimeType) {
+    return mimeTypeMap.get(mimeType);
+  }
+}
