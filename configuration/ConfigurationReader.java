2b2dad67b27bebd3ab6f7a70c41b3f4324f3d569
James Andrews <bdaddyja@gmail.com>
2018-09-10 14:07:34 -0700

    ConfigurationReader returns instances of configs in a Factory pattern.

--- configuration/ConfigurationReader.java
+++ configuration/ConfigurationReader.java
@@ -1,33 +1,20 @@
+
+package configuration;
+
 import java.util.*;
-import java.util.Properties;
 
-class ConfigurationReader {
-  private Properties configFile;
-  private ClassLoader classLoader = getClass().getClassLoader();
+public class ConfigurationReader {
 
-  public ConfigurationReader(String config) {
-	  this.configFile = new Properties();
-
-    try {
-  	  this.configFile.load(classLoader.getResourceAsStream(config));
-  	}catch(Exception e){
-  	  e.printStackTrace();
+  public static Config getConfig(String config) {
+    //It works.  Will test more.
+    //Need to add rest of configuration files
+    switch(config) {
+      case "HTTPD_CONF":
+        return new HttpdConf("conf/httpd.conf");
+      case "MIME_TYPE":
+        return new MimeTypes("conf/mime.types");
+      default:
+        return null;
     }
-  }
-
-  public String getProperty(String key) {
-  	String value = this.configFile.getProperty(key);
-  	return value;
-  }
-
-  public Set<String> getProps() {
-    return this.configFile.stringPropertyNames();
-  }
-
-  public static void main(String[] args) {
-    //Do configuration reader stuff
-    ConfigurationReader test = new ConfigurationReader("conf/mime.types");
-    System.out.println(test.getProperty("application/andrew-inset"));
-    System.out.println(test.getProps());
   }
 }
