391852c4579d9e4d42bdd6ac85fc18dca79a5f60
James Andrews <bdaddyja@gmail.com>
2018-09-11 16:37:31 -0700

    Skeleton for Htaxxess and Htpassword.

---
+++ configuration/Htaccess.java
@@ -0,0 +1,9 @@
+
+package configuration;
+
+public class Htaccess implements Config {
+  public void load() {
+    //all the loads should load up class with needed info
+    System.out.println("james");
+  }
+}
