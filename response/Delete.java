ef96ff78bd84a4ea859cc49524b8dfce20f92724
James Andrews <bdaddyja@gmail.com>
2018-09-12 22:59:57 -0700

    Skeleton for response.

---
+++ response/Delete.java
@@ -0,0 +1,18 @@
+package response;
+
+public class Delete extends Response {
+
+  private static int statusCode = 204;
+  //Deletes file
+  public Delete() {
+
+  }
+
+  public Response getResponse(){
+    return null;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("james");
+  }
+}
