da53b384807c8e6d7eb877c9930130e05b42daf1
James Andrews <bdaddyja@gmail.com>
2018-09-16 16:01:37 -0700

    Skeleton for basic response.  Still need to implement.

---
+++ response/ResponseTest.java
@@ -0,0 +1,20 @@
+
+import java.io.OutputStream;
+
+public class ResponseTest {
+
+  private static int statusCode = 200;
+  private String reasonPhrase;
+  private Resource resource;
+
+  public ResponseTest(Resource test) {
+    return null;
+  }
+  public OutputStream response(OutputStream stream) {
+    return null;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("James");
+  }
+}
