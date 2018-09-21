3a3fefef067559a2b0043d22e885d1acc0e76d7a
Michael <michaelrogerswanson@gmail.com>
2018-09-20 20:00:56 -0700

    able to send common log formatted strings to log files, need to hook up input fields

--- logger/Logger.java
+++ logger/Logger.java
@@ -1,8 +1,10 @@
 
 package logger;
 
+import java.io.IOException;
 import java.io.FileWriter;
-import java.io.RrintWriter;
+import java.io.PrintWriter;
+import java.util.Date;
 
 import configuration.*;
 import resource.*;
@@ -14,9 +16,20 @@ public class Logger {
   private Config httpdConf;
   private String logFile;
 
-  public Logger(String httpdConf) {
-    this.httpdConf = this.configFactory.getConfig(httpdConf);
+  public Logger() {
+    this.httpdConf = this.configFactory.getConfig("HTTPD_CONF");
     this.logFile = this.httpdConf.lookUp("LogFile", "HTTPD_CONF");
+  }
+
+  public Logger(int test) {
+    this.httpdConf = this.configFactory.getConfig("HTTPD_CONF");
+    this.logFile = this.httpdConf.lookUp("LogFile", "HTTPD_CONF");
+
+    try {
+      this.log();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   public void log(Request request, Resource resource)
@@ -31,4 +44,26 @@ public class Logger {
     printWriter.close();
   }
 
+  public void log() throws IOException {
+    // TODO write in common log format
+    // https://en.wikipedia.org/wiki/Common_Log_Format
+
+    FileWriter fileWriter = new FileWriter(this.logFile, true);
+    PrintWriter printWriter = new PrintWriter(fileWriter);
+
+    Date currentDate = new Date();
+
+    // Common_Log_Format: IP identifier user [date:time tz] verb uri version status size
+    printWriter.printf("%s %s %s [%s/%s/%s:%s:%s:%s %s] %s %s %s %s %s\n",
+      "testIP", "testIdentifier", "testUser",
+      "testDay", "testMonth", "testYear", "testHour",
+      "testMin", "testSecond", "testTZ",
+      "testVerb", "testURI", "testVersion",
+      "testStatus", "testSize");
+    printWriter.close();
+  }
+
+  public static void main(String[] args) {
+    Logger logger = new Logger(-1);
+  }
 }
