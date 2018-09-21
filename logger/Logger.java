025d7f5c7cf58782596a81d2b35649773d38240d
Michael Swanson <michaelrogerswanson@gmail.com>
2018-09-20 23:18:21 -0700

    added timestamping, logger fully functional, still needs inputs from request/ response

--- logger/Logger.java
+++ logger/Logger.java
@@ -4,7 +4,12 @@ package logger;
 import java.io.IOException;
 import java.io.FileWriter;
 import java.io.PrintWriter;
-import java.util.Date;
+import java.time.ZonedDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+import java.lang.StringBuffer;
+
+
 
 import configuration.*;
 import resource.*;
@@ -34,33 +39,37 @@ public class Logger {
 
   public void log(Request request, Resource resource)
     throws IOException {
-    // TODO write in common log format
-    // https://en.wikipedia.org/wiki/Common_Log_Format
+      FileWriter fileWriter = new FileWriter(this.logFile, true);
+      PrintWriter printWriter = new PrintWriter(fileWriter);
 
-    FileWriter fileWriter = new FileWriter(this.logFile);
-    PrintWriter printWriter = new PrintWriter(fileWriter);
-
-    printWriter.println("If you can read this then it works!");
-    printWriter.close();
+      // Common_Log_Format: IP identifier user [date:time tz] verb uri version status size
+      printWriter.printf("%s %s %s [%s] %s %s %s %s %s\n",
+        "testIP", "testIdentifier", "testUser",
+        this.getDateTime(ZonedDateTime.now()),
+        "testVerb", "testURI", "testVersion",
+        "testStatus", "testSize");
+      printWriter.close();
   }
 
   public void log() throws IOException {
-    // TODO write in common log format
-    // https://en.wikipedia.org/wiki/Common_Log_Format
-
     FileWriter fileWriter = new FileWriter(this.logFile, true);
     PrintWriter printWriter = new PrintWriter(fileWriter);
 
-    Date currentDate = new Date();
-
     // Common_Log_Format: IP identifier user [date:time tz] verb uri version status size
-    printWriter.printf("%s %s %s [%s/%s/%s:%s:%s:%s %s] %s %s %s %s %s\n",
+    printWriter.printf("%s %s %s [%s] %s %s %s %s %s\n",
       "testIP", "testIdentifier", "testUser",
-      "testDay", "testMonth", "testYear", "testHour",
-      "testMin", "testSecond", "testTZ",
+      this.getDateTime(ZonedDateTime.now()),
       "testVerb", "testURI", "testVersion",
       "testStatus", "testSize");
     printWriter.close();
+  }
+
+  private String getDateTime(ZonedDateTime timeStamp) {
+    return String.format("%02d/%s/%04d:%02d:%02d:%02d %s",
+    timeStamp.getDayOfMonth(),
+    timeStamp.getMonth().getDisplayName(TextStyle.SHORT,Locale.ENGLISH),
+    timeStamp.getYear(), timeStamp.getHour(), timeStamp.getMinute(),
+    timeStamp.getSecond(), timeStamp.getOffset());
   }
 
   public static void main(String[] args) {
