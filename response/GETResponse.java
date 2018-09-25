2e13dfc855b6edd3074a37c2e169cb3ded0cac63
James Andrews <bdaddyja@gmail.com>
2018-09-25 14:18:19 -0700

    GET response done.

--- response/GETResponse.java
+++ response/GETResponse.java
@@ -8,7 +8,6 @@ import java.util.Date;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.IOException;
-import java.io.FilterOutputStream;
 import java.io.OutputStream;
 import java.io.File;
 import java.io.FileReader;
@@ -46,33 +45,4 @@ public class GETResponse extends Response {
       out.close();
     }
   }
-
-  public byte[] getResponseHeaders() throws IOException {
-    StringBuilder headers = new StringBuilder();
-    Date localDate = new Date();
-
-    headers.append(this.request.getVersion());
-    headers.append(" ");
-    headers.append(this.statusCode);
-    headers.append(" ");
-    headers.append(this.reasonPhrase);
-    headers.append("\n");
-    headers.append("Date: ");
-    headers.append(localDate);
-    headers.append("\n");
-    headers.append("Server: FireSquad/1.0");
-    headers.append("\n");
-    headers.append("Status: 200 OK");
-    headers.append("\n");
-    headers.append("Content-Type: " + this.getContenType());
-    headers.append("\n");
-    headers.append("Content-Length: " + this.getResource().length);
-    headers.append("\n");
-    headers.append("\n");
-
-    byte[] string = headers.toString().getBytes();
-
-    return string;
-  }
-
 }
