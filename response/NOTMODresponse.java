c48a866aa5a6a8c1ffacfe70164c2021695417d6
James Andrews <bdaddyja@gmail.com>
2018-09-25 22:21:38 -0700

    Notmod response.

---
+++ response/NOTMODresponse.java
@@ -0,0 +1,30 @@
+
+package response;
+
+import resource.*;
+import request.*;
+import configuration.*;
+import java.util.Date;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+
+public class NOTMODresponse extends Response {
+
+  public NOTMODresponse(Resource resource) throws IOException {
+    this.resource = resource;
+    this.request = resource.getRequest();
+    this.statusCode = 304;
+    this.reasonPhrase = "Not Modified";
+  }
+
+  public void send(OutputStream out) throws IOException {
+    out.write(this.get304ResponseHeaders());
+    out.flush();
+    out.close();
+  }
+}
